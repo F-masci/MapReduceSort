80007a962bc11c8b762a0429c6e3906e06da0b3a
F-masci <francesco.masci0407@libero.it>
2024-12-11 16:06:29 +0100

    Fix & comments #2

--- config/config.go
+++ config/config.go
@@ -4,24 +4,30 @@ import (
 	"MapReduceSort/structs"
 	"MapReduceSort/utils"
 	"encoding/json"
+	"fmt"
 	"os"
 )
 
 const (
-	CasualNumbersDim   = 1000
-	CasualNumbersRange = 10000
+	CasualNumbersDim   = 1000  // Dimension of the random numbers array
+	CasualNumbersRange = 10000 // Maximum range for the random numbers
 )
 
-// Funzione per leggere il file JSON e fare il parsing
+// loadAddresses Reads the JSON file and performs parsing
 func loadAddresses(filename string, target interface{}) error {
-	// Leggi il file JSON
+	// Open the JSON file
 	file, err := os.Open(filename)
 	if err != nil {
-		return err
+		return err // Return error if file cannot be opened
 	}
-	defer file.Close()
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			_ = fmt.Errorf("error closing the file: %v", err)
+		}
+	}(file)
 
-	// Decodifica il contenuto JSON
+	// Decode the JSON content
 	decoder := json.NewDecoder(file)
 	err = decoder.Decode(target)
 	if err != nil {
@@ -31,14 +37,18 @@ func loadAddresses(filename string, target interface{}) error {
 	return nil
 }
 
+// ParseConfig Parses the configuration files for master, mapper, and reducer addresses
 func ParseConfig() (masterAddresses []structs.MasterAddress, mapperAddresses []structs.WorkerAddress, reducerAddresses []structs.WorkerAddress) {
 
+	// Load master addresses from JSON file
 	err := loadAddresses("config/master.json", &masterAddresses)
 	utils.CheckError(err)
 
+	// Load mapper addresses from JSON file
 	err = loadAddresses("config/mapper.json", &mapperAddresses)
 	utils.CheckError(err)
 
+	// Load reducer addresses from JSON file
 	err = loadAddresses("config/reducer.json", &reducerAddresses)
 	utils.CheckError(err)
 
